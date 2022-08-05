cd9744331a5a4e33b6018d1184a3039be062ebd3
MattTitmas <90630353+MattTitmas@users.noreply.github.com>
2022-08-05 22:04:27 +0100

    Update README.md and add argparse

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # number_recognition
-Number recognition using a neural network
+### Number recognition using a neural network
+
+## Installation:
+Python dependencies:
+```
+pip install -r requirements.txt
+```
+
+## Usage
+```
+python -m main -h
+```
+Output:
+```
+usage: main.py [-h] -t TRAIN -o OUTPUT [-e EPOCHS] [-hn HIDDEN_NODES [HIDDEN_NODES ...]]
+
+Train a number recognition model.
+
+options:
+  -h, --help            show this help message and exit
+  -t TRAIN, --train TRAIN
+                        Location of training file.
+  -o OUTPUT, --output OUTPUT
+                        Directory of output weights.
+  -e EPOCHS, --epochs EPOCHS
+                        Number of epochs to train with.
+  -hn HIDDEN_NODES [HIDDEN_NODES ...], --hidden_nodes HIDDEN_NODES [HIDDEN_NODES ...]
+                        Number of hidden nodes per layer.
+
+```
--- main.py
+++ main.py
@@ -1,25 +1,42 @@
-from neural_network import NeuralNetwork
-import numpy as np
 import csv
+import argparse
+from typing import List
+
+import numpy as np
 from tqdm import tqdm
 
-if __name__ == '__main__':
-    AI = NeuralNetwork(784, 10, [512, 256, 128])
+from neural_network import NeuralNetwork
 
-    epochs = 3
-    for i in tqdm(range(epochs)):
-        with open('data/train.csv', 'r') as training_data:
+
+def main():
+    parser = argparse.ArgumentParser(description='Train a number recognition model.')
+    parser.add_argument('-t', '--train', type=str, required=True,
+                        help='Location of training file.')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Directory of output weights.')
+    parser.add_argument('-e', '--epochs', type=int, required=False, default=3,
+                        help='Number of epochs to train with.')
+    parser.add_argument('-hn', '--hidden_nodes', nargs='+', type=List[int], required=False, default=[512, 256, 128],
+                        help='Number of hidden nodes per layer.')
+
+    args = parser.parse_args()
+    number_recognition_model = NeuralNetwork(784, 10, args.hidden_nodes)
+
+    for _ in tqdm(range(args.epochs)):
+        with open(args.train, 'r') as training_data:
             datareader = csv.reader(training_data)
             for c, row in enumerate(tqdm(datareader, total=60000, leave=False)):
-                data = np.where(np.array(row[1:]).astype(int) < 127, 0, 1).reshape(1, 28*28)
+                data = np.where(np.array(row[1:]).astype(int) < 127, 0, 1).reshape(1, 28 * 28)
                 expected = np.zeros(10)
                 expected[int(row[0])] = 1
-                AI.train(data, expected)
+                number_recognition_model.train(data, expected)
 
-    for c, i in enumerate(AI.get_weights):
-        np.save('data/weights/weight'+str(c)+'.npy', i)
+    for c, i in enumerate(number_recognition_model.get_weights):
+        np.save(args.output + str(c) + '.npy', i)
+
+    for c, i in enumerate(number_recognition_model.get_bias):
+        np.save(args.output + str(c) + '.npy', i)
 
 
-    for c, i in enumerate(AI.get_bias):
-        np.save('data/weights/bias'+str(c)+'.npy', i)
-
+if __name__ == '__main__':
+    main()
